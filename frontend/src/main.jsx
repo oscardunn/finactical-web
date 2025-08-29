56bccc9c17a077529c83da80987430d5f61202ed
Oscar Dunn <oscardunn1998@gmail.com>
2025-08-29 13:15:51 +0100

    releative paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "/Users/uncleosk/Finactical-web/finactical-web/frontend/src/app.jsx";
+import App from "frontend/src/App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
